49f1e2d196c7ada8bd07d0698b83601b1110e17a
Mirah Gary <mgary@MacBook-5.local>
2017-08-02 18:47:38 +0200

    Created bun package and tests

---
+++ bun/bun.go
@@ -0,0 +1,12 @@
+package bun
+
+import (
+	"errors"
+)
+
+type Bun struct {
+	Location locationtype
+	Size int
+	Picture pic
+	Description string
+}
---
+++ bun/bun_test.go
@@ -0,0 +1,5 @@
+package bun
+
+import (
+       "testing"
+)
